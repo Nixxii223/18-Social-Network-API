e2caaa551a9ab9a27672158080628c2c70181427
nickib223 <146459728+nickib223@users.noreply.github.com>
2024-03-13 14:32:19 -0400

    added file structure and initial controllers

---
+++ controllers/userController.js
@@ -0,0 +1,54 @@
+// const { User } = require('../models');
+
+module.exports = {
+    async getAllUsers(req, res) {
+        try {
+            const users = await User.find({});
+            res.json(users);
+        } catch (err) {
+            console.error(err);
+            res.status(400).json(err);
+        }
+    },
+
+    async getUserById({ params }, res) {
+        try {
+            const user = await User.findOne({ _id: params.id });
+            res.json(user);
+        } catch (err) {
+            console.error(err);
+            res.status(400).json(err);
+        }
+    },
+
+    async createUser({ body }, res) {
+        try {
+            const user = await User.create(body);
+            res.json(user);
+        } catch (err) {
+            console.error(err);
+            res.status(400).json(err);
+        }
+    },
+    
+    async deleteUser({ params }, res) {
+        try {
+            const user = await User.findOneAndDelete({ _id: params.id });
+            res.json(user);
+        } catch (err) {
+            console.error(err);
+            res.status(400).json(err);
+        }
+    },
+
+    async updateUser({ params, body }, res) {
+        try {
+            const user = await User.findOneAndUpdate({ _id: params.id },
+                body, { new: true, runValidators: true });
+            res.json(user);
+        } catch (err) {
+            console.error(err);
+            res.status(400).json(err);
+        }
+    }
+};    
---
+++ models/Thought.js
@@ -0,0 +1,20 @@
+// const assignmentSchema = require('./Reaction');
+// const studentSchema = new Schema(
+//     {
+//       first: {
+//         type: String,
+//         required: true,
+//         max_length: 50,
+//       },
+//       last: {
+//         type: String,
+//         required: true,
+//         max_length: 50,
+//       },
+//       github: {
+//         type: String,
+//         required: true,
+//         max_length: 50,
+//       },
+//       assignments: [assignmentSchema], reaction:[reactionSchema]
+//     },
