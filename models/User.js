656c5b79ca163d093f6c514a3bf5feb32626b2a0
nickib223 <146459728+nickib223@users.noreply.github.com>
2024-03-14 21:04:02 -0400

    updated user route

--- models/User.js
+++ models/User.js
@@ -25,7 +25,7 @@ const UserSchema = new Schema(
     }
 );
 
-UserSchemaSchema.virtual('friendCount').get(function() {
+UserSchema.virtual('friendCount').get(function() {
     return this.friends.length;
 });
 
